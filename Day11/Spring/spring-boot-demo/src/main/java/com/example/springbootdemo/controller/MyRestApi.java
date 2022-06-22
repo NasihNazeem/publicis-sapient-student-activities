355d93292e2c9c6a7224e5e71a01ab62dad52790
Nasih Mohamed Nazeem <nasih.mohamednazeem@ucalgary.ca>
2022-06-22 16:47:50 -0400

    Practice Post, Get and DeleteMapping

--- Day11/Spring/spring-boot-demo/src/main/java/com/example/springbootdemo/controller/MyRestApi.java
+++ Day11/Spring/spring-boot-demo/src/main/java/com/example/springbootdemo/controller/MyRestApi.java
@@ -1,10 +1,17 @@
 package com.example.springbootdemo.controller;
 
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,8 +35,57 @@ public class MyRestApi {
     }
 
     @GetMapping
-    public List<User> fetchUsers(){
+    public Map<Integer,User> fetchUsers(){
         return service.getAllUsers();
     }
 
+    @GetMapping("/{userId}")
+    public ResponseEntity<Object> getUser(@PathVariable("userId") int id){
+        User user = new User();
+        Map<String, String> map = new HashMap<>();
+        user = service.getUser(id);
+
+        if(user == null){
+            map.put("error", "sorry user with an id " + id + " not found");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(map.get("error"));
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(user);
+    }
+
+    @PostMapping("/{userId}/{dob}")
+    public ResponseEntity<Object> updateUserDoB(@PathVariable("userId") int id, @PathVariable("dob") String dateofbirth){
+        User user = new User();
+        Map<String, String> map = new HashMap<>();
+
+        user = service.getUser(id);
+        
+
+        if(user != null){
+            user.setDob(LocalDate.parse(dateofbirth));
+            return ResponseEntity.status(HttpStatus.OK).body(user);
+        }else{
+            map.put("error", "sorry user with an id " + id + " not found");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(map.get("error"));
+        }
+
+        
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<Object> deleteUser(@PathVariable("userId") int id){
+        User user = new User();
+        Map<String, String> map = new HashMap<>();
+
+        user = service.deleteUser(id);
+        
+        if(user == null){
+            map.put("message", id + " is deleted");
+            return ResponseEntity.status(HttpStatus.OK).body(map.get("message"));
+        } else {
+            map.put("error", "sorry user with an id " + id + " not found");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(map.get("error"));
+        }
+
+    }
 }
--- Day11/Spring/spring-boot-demo/src/main/java/com/example/springbootdemo/model/service/UserServiceImpl.java
+++ Day11/Spring/spring-boot-demo/src/main/java/com/example/springbootdemo/model/service/UserServiceImpl.java
@@ -2,28 +2,51 @@ package com.example.springbootdemo.model.service;
 
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.websocket.server.ServerEndpoint;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.springbootdemo.model.beans.User;
 
 
 @Service
 public class UserServiceImpl {
-    private static List<User> userList = new ArrayList<>();
+    private static Map<Integer,User> userList = new HashMap<>();
 
     public User storeUser(User user) {
         int counter = userList.size();
         user.setUserId(++counter);
-        userList.add(user);
+        userList.put(counter,user);
 
         return user;
     }
 
-    public List<User> getAllUsers(){
+    public Map<Integer, User> getAllUsers(){
         return userList;
+    }
+
+    public User getUser(int id){
+
+        if((id != 0) || id < userList.size())
+            return userList.get(id);
+        else
+            return null;
+    }
+
+    public User deleteUser(int id){
+        if( id != 0 || id < userList.size()){
+            userList.remove(id);
+            return null;
+        } else{
+            return userList.get(id);
+        }
+            
     }
 }
